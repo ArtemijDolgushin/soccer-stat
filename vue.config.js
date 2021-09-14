bbb30bdbc974d5fffba91fbc28a9ea003a64d337
boo-mer <artemijdolgushin@gmail.com>
2021-09-14 06:54:44 +0300

    enable deployment to github pages

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,7 @@
 process.env.VUE_APP_API_TOKEN = '';
 process.env.VUE_APP_API_AVAILABLE_TIERS = 'TIER_ONE';
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/soccer-stat/'
+        : '/'
+}
